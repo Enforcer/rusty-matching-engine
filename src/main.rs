812dcdbdae0c19171f2ccdcfcb95da55480496bc
Sebastian Buczyński <nnplaya@gmail.com>
2019-09-24 20:11:52 +0200

    Implement simple main loop

--- src/main.rs
+++ src/main.rs
@@ -1,168 +1,278 @@
 #[macro_use]
-extern crate intrusive_collections;
+extern crate text_io;
 
 use std::cmp::min;
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::collections::VecDeque;
+use std::str::FromStr;
 
 #[derive(Copy, Clone, PartialEq, Debug)]
 enum Side {
-	Bid,
-	Ask
+    Bid,
+    Ask,
 }
 
 #[derive(Copy, Clone, Debug)]
 struct Trade {
-	executing_order_id: i32,
-	matched_order_id: i32,
-	timestamp: u128,
-	amount: i32,
-	price: i32
+    executing_order_id: i32,
+    matched_order_id: i32,
+    timestamp: u128,
+    amount: i32,
+    price: i32,
 }
 
 #[derive(Copy, Clone, Debug)]
 struct Order {
-	side: Side,
-	amount: i32,
-	price: i32,
-	timestamp: i32
+    side: Side,
+    amount: i32,
+    price: i32,
+    timestamp: i32,
 }
 
 impl Order {
-	fn matches(&self, other: &Self) -> bool {
-		return (self.side == Side::Bid && self.price >= other.price) || (self.side == Side::Ask && self.price <= other.price);
-	}
+    fn matches(&self, other: &Self) -> bool {
+        return (self.side == Side::Bid && self.price >= other.price)
+            || (self.side == Side::Ask && self.price <= other.price);
+    }
 }
 
 impl Ord for Order {
-	fn cmp(&self, other: &Self) -> Ordering {
-		let multiplier;
-		if self.side == Side::Ask {
-			multiplier = -1;
-		} else {
-			multiplier = 1;
-		}
-		(self.price, self.timestamp).cmp(&((other.price * multiplier), other.timestamp))
-	}
+    fn cmp(&self, other: &Self) -> Ordering {
+        let multiplier;
+        if self.side == Side::Ask {
+            multiplier = -1;
+        } else {
+            multiplier = 1;
+        }
+        (self.price, self.timestamp).cmp(&((other.price * multiplier), other.timestamp))
+    }
 }
 
 impl PartialOrd for Order {
-	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-		Some(self.cmp(other))
-	}
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 impl PartialEq for Order {
-	fn eq(&self, other: &Self) -> bool {
-		self.price == other.price && self.timestamp == other.timestamp
-	}
+    fn eq(&self, other: &Self) -> bool {
+        self.price == other.price && self.timestamp == other.timestamp
+    }
 }
 
 impl Eq for Order {}
 
+impl FromStr for Order {
+    type Err = std::num::ParseIntError;
 
-fn main() {
-	let mut asks = BinaryHeap::<Order>::new();
-	asks.push(Order{ side: Side::Ask, amount: 200, price: 2030, timestamp: 905 });
-	asks.push(Order{ side: Side::Ask, amount: 100, price: 2030, timestamp: 901 });
-	asks.push(Order{ side: Side::Ask, amount: 100, price: 2025, timestamp: 903 });
-	println!("Top ask: {:?}", asks.peek().unwrap());
-
-	let bids = BinaryHeap::<Order>::new();
-
-	let (asks, bids, trades_1) = execute_limit_order(asks, bids, Order{ side: Side::Bid, amount: 250, price: 2035, timestamp: 908 });
-	let (asks, bids, trades_2) = execute_limit_order(asks, bids, Order{ side: Side::Ask, amount: 250, price: 2035, timestamp: 908 });
-
-	println!("Asks after: {:?}", asks.into_sorted_vec());
-	println!("Bids after: {:?}", bids.into_sorted_vec());
-	println!("Trades generated: {:?} and {:?}", trades_1, trades_2);
+    fn from_str(raw_str: &str) -> Result<Self, Self::Err> {
+        let (side_int, amount, price, timestamp): (i32, i32, i32, i32);
+        scan!(raw_str.bytes() => "{} {} {} {}", side_int, amount, price, timestamp);
+        let side;
+        if side_int == 4 {
+            side = Side::Ask;
+        } else if side_int == 8 {
+            side = Side::Bid;
+        } else {
+            panic!("Invalid side")
+        }
+        Ok(Self {
+            side: side,
+            amount: amount,
+            price: price,
+            timestamp: timestamp,
+        })
+    }
 }
 
-fn execute_limit_order(asks: BinaryHeap::<Order>, bids: BinaryHeap::<Order>, mut new_order: Order) -> (BinaryHeap::<Order>, BinaryHeap::<Order>, VecDeque::<Trade>) {
-	// TODO: return trades
-	// TODO: order executing strategies: LIMIT, MARKET, STOP
-	// TODO: time in force - GTC, FOK, IOC
-	let (mut same_side, mut other_side) = if new_order.side == Side::Bid {
-		(bids, asks)
-	} else {
-		(asks, bids)
-	};
-	let mut trades = VecDeque::<Trade>::new();
+fn main() {
+    let mut asks = BinaryHeap::<Order>::new();
+    let mut bids = BinaryHeap::<Order>::new();
+    loop {
+        let line: String = read!("{}\n");
+        match Order::from_str(&line) {
+            Ok(new_order) => {
+                let trades = execute_limit_order(&mut asks, &mut bids, new_order);
+                println!("Trades generated: {:?}", trades);
+                for order in asks.iter() {
+                    println!("Ask: {:?}", order);
+                }
+                for order in bids.iter() {
+                    println!("Ask: {:?}", order);
+                }
+            }
+            Err(_) => {
+                println!("Couldn't parse input: '{}'", line);
+            }
+        }
+    }
+}
 
-	while new_order.amount > 0 && other_side.peek() != None && new_order.matches(&(other_side.peek().unwrap())) {
-		let matched_order = other_side.peek().unwrap();
-		let matched_amount = min(new_order.amount, matched_order.amount);
-		let price = matched_order.price;
-		new_order.amount -= matched_amount;
-		// if other order is filled, remove it
-		if matched_amount == matched_order.amount {
-			let ask_to_delete = other_side.pop();
-			println!("Filled! {:?}", ask_to_delete);
-		} else { // otherwise, lower amount only
-			other_side.peek_mut().unwrap().amount -= matched_amount;
-		}
-		let now = std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH).expect("WTF");
-		trades.push_back(Trade{ executing_order_id: 1, matched_order_id: 1, timestamp: now.as_nanos(), amount: matched_amount, price: price });
-	}
-	if new_order.amount > 0 { // IoC wouldn't add it
-		println!("Pushing to same side {:?}", new_order);
-		same_side.push(new_order);
-	} else {
-		println!("Filled! {:?}", new_order);
-	}
+fn execute_limit_order(
+    asks: &mut BinaryHeap<Order>,
+    bids: &mut BinaryHeap<Order>,
+    mut new_order: Order,
+) -> (VecDeque<Trade>) {
+    // TODO: order executing strategies: LIMIT, MARKET, STOP
+    // TODO: time in force - GTC, FOK, IOC
+    let (same_side, other_side) = if new_order.side == Side::Bid {
+        (bids, asks)
+    } else {
+        (asks, bids)
+    };
+    let mut trades = VecDeque::<Trade>::new();
 
-	if new_order.side == Side::Bid {
-		(other_side, same_side, trades)
-	} else {
-		(same_side, other_side, trades)
-	}
+    while new_order.amount > 0
+        && other_side.peek() != None
+        && new_order.matches(&(other_side.peek().unwrap()))
+    {
+        let matched_order = other_side.peek().unwrap();
+        let matched_amount = min(new_order.amount, matched_order.amount);
+        let price = matched_order.price;
+        new_order.amount -= matched_amount;
+        // if other order is filled, remove it
+        if matched_amount == matched_order.amount {
+            let ask_to_delete = other_side.pop();
+            println!("Filled! {:?}", ask_to_delete);
+        } else {
+            // otherwise, lower amount only
+            other_side.peek_mut().unwrap().amount -= matched_amount;
+        }
+        let now = std::time::SystemTime::now()
+            .duration_since(std::time::SystemTime::UNIX_EPOCH)
+            .expect("WTF");
+        trades.push_back(Trade {
+            executing_order_id: 1,
+            matched_order_id: 1,
+            timestamp: now.as_nanos(),
+            amount: matched_amount,
+            price: price,
+        });
+    }
+    if new_order.amount > 0 {
+        // IoC wouldn't add it
+        println!("Pushing to same side {:?}", new_order);
+        same_side.push(new_order);
+    } else {
+        println!("Filled! {:?}", new_order);
+    }
+
+    trades
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn test_cross_order_bid() {
-		let asks = BinaryHeap::from(vec![Order{ side: Side::Ask, amount: 10, price: 10, timestamp: 1 }]);
-		let bids = BinaryHeap::<Order>::new();
+    #[test]
+    fn test_cross_order_bid() {
+        let mut asks = BinaryHeap::from(vec![Order {
+            side: Side::Ask,
+            amount: 10,
+            price: 10,
+            timestamp: 1,
+        }]);
+        let mut bids = BinaryHeap::<Order>::new();
 
-		let (asks, bids, trades) = execute_limit_order(asks, bids, Order{ side: Side::Bid, amount: 10, price: 10, timestamp: 1 });
+        let trades = execute_limit_order(
+            &mut asks,
+            &mut bids,
+            Order {
+                side: Side::Bid,
+                amount: 10,
+                price: 10,
+                timestamp: 1,
+            },
+        );
 
-		assert_eq!(asks.into_sorted_vec(), []);
-		assert_eq!(bids.into_sorted_vec(), []);
-		assert_has_one_trade(trades, 10, 10);
-	}
+        assert_eq!(asks.into_sorted_vec(), []);
+        assert_eq!(bids.into_sorted_vec(), []);
+        assert_has_one_trade(trades, 10, 10);
+    }
 
-	#[test]
-	fn test_cross_order_ask() {
-		let asks = BinaryHeap::<Order>::new();
-		let bids = BinaryHeap::from(vec![Order{ side: Side::Bid, amount: 10, price: 10, timestamp: 1 }]);
+    #[test]
+    fn test_cross_order_ask() {
+        let mut asks = BinaryHeap::<Order>::new();
+        let mut bids = BinaryHeap::from(vec![Order {
+            side: Side::Bid,
+            amount: 10,
+            price: 10,
+            timestamp: 1,
+        }]);
 
-		let (asks, bids, trades) = execute_limit_order(asks, bids, Order{ side: Side::Ask, amount: 10, price: 10, timestamp: 1 });
+        let trades = execute_limit_order(
+            &mut asks,
+            &mut bids,
+            Order {
+                side: Side::Ask,
+                amount: 10,
+                price: 10,
+                timestamp: 1,
+            },
+        );
 
-		assert_eq!(asks.into_sorted_vec(), []);
-		assert_eq!(bids.into_sorted_vec(), []);
-		assert_has_one_trade(trades, 10, 10);
-	}
+        assert_eq!(asks.into_sorted_vec(), []);
+        assert_eq!(bids.into_sorted_vec(), []);
+        assert_has_one_trade(trades, 10, 10);
+    }
 
-	#[test]
-	fn test_cheaper_ask_comes_in() {
-		let asks = BinaryHeap::<Order>::new();
-		let bids = BinaryHeap::from(vec![Order{ side: Side::Bid, amount: 10, price: 10, timestamp: 1 }]);
+    #[test]
+    fn test_cheaper_ask_comes_in() {
+        let mut asks = BinaryHeap::<Order>::new();
+        let mut bids = BinaryHeap::from(vec![Order {
+            side: Side::Bid,
+            amount: 10,
+            price: 10,
+            timestamp: 1,
+        }]);
 
-		let (asks, bids, trades) = execute_limit_order(asks, bids, Order{ side: Side::Ask, amount: 10, price: 5, timestamp: 1 });
+        let trades = execute_limit_order(
+            &mut asks,
+            &mut bids,
+            Order {
+                side: Side::Ask,
+                amount: 10,
+                price: 5,
+                timestamp: 1,
+            },
+        );
 
-		assert_eq!(asks.into_sorted_vec(), []);
-		assert_eq!(bids.into_sorted_vec(), []);
-		assert_has_one_trade(trades, 10, 10);
-	}
+        assert_eq!(asks.into_sorted_vec(), []);
+        assert_eq!(bids.into_sorted_vec(), []);
+        assert_has_one_trade(trades, 10, 10);
+    }
 
-	fn assert_has_one_trade(trades: VecDeque::<Trade>, amount: i32, price: i32) {
-		assert_eq!(trades.len(), 1);
-		let only_trade = trades.front().unwrap();
-		assert_eq!(only_trade.amount, amount);
-		assert_eq!(only_trade.price, price);
-	}
+    fn assert_has_one_trade(trades: VecDeque<Trade>, amount: i32, price: i32) {
+        assert_eq!(trades.len(), 1);
+        let only_trade = trades.front().unwrap();
+        assert_eq!(only_trade.amount, amount);
+        assert_eq!(only_trade.price, price);
+    }
+
+    #[test]
+    fn test_order_from_str_bid() {
+        assert_eq!(
+            Order::from_str("8 1 2 0"),
+            Ok(Order {
+                side: Side::Bid,
+                amount: 1,
+                price: 2,
+                timestamp: 0
+            })
+        );
+    }
+
+    #[test]
+    fn test_order_from_str_ask() {
+        assert_eq!(
+            Order::from_str("4 9 1 2"),
+            Ok(Order {
+                side: Side::Bid,
+                amount: 9,
+                price: 1,
+                timestamp: 2
+            })
+        );
+    }
 }
